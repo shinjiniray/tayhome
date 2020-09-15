4e0ba1577d7c2dadfcd872bd0056cede052993e2
shinjiniray <shinjini.ray@gmail.com>
2020-09-16 00:43:50 +0530

    initial commit

---
+++ webhook/controller.py
@@ -0,0 +1,45 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET', 'POST'])
+def webhook(request):
+    reply_msg = ""
+    endsession = True
+
+    if request.data['request']['type'] == 'LaunchRequest':
+        reply_msg = "Hi, I am your home nigga"
+        endsession = False
+    elif request.data['request']['type'] == 'IntentRequest':
+        if request.data['request']['intent']['name'] == 'turnon':
+            reply_msg = "turning on nigga"
+            endsession = True
+        elif request.data['request']['intent']['name'] == 'turnoff':
+            reply_msg = "turning on nigga"
+            endsession = True
+        elif request.data['request']['intent']['name'] == 'AMAZON.StopIntent':
+            endsession = True
+
+    response_body = {
+        "version": "1.0",
+        "response": {
+            "outputSpeech": {
+                "type": "PlainText",
+                "text": reply_msg
+            },
+            "card": {
+                "type": "Simple",
+                "title": "Lima Chat",
+                "content": reply_msg
+            },
+            "reprompt": {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": "Please give me further information"
+                }
+            },
+            "shouldEndSession": endsession
+        }
+    }
+
+    return Response(response_body)
